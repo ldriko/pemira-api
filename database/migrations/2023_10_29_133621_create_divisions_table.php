d6bcc8b23fe4ad0ecbdf721e0288770e0f23ef49
jerryrc <132052046+JERRY-RAMADHANI@users.noreply.github.com>
2023-10-29 21:13:20 +0700

    migration

--- database/migrations/2023_10_29_133621_create_divisions_table.php
+++ database/migrations/2023_10_29_133621_create_divisions_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('divisions', function (Blueprint $table) {
             $table->id();
-            $table->string('npm');
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->tinyInteger('type');
             $table->string('name');
-            $table->string('picture')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->tinyInteger('role')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('divisions');
     }
 };
---
+++ database/migrations/2023_10_29_134554_create_ballots_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ballots', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->string('npm');
+            $table->string('ktm_picture');
+            $table->string('verification_picture');
+            $table->unsignedBigInteger('candidate1');
+            $table->foreign('candidate1')->references('id')->on('candidates');
+            $table->unsignedBigInteger('candidate2');
+            $table->foreign('candidate2')->references('id')->on('candidates');
+            $table->unsignedBigInteger('candidate3');
+            $table->foreign('candidate3')->references('id')->on('candidates');
+            $table->unsignedBigInteger('candidate4');
+            $table->foreign('candidate4')->references('id')->on('candidates');
+            $table->tinyInteger('accepted')->nullable();
+            $table->timestamp('accepted_at')->nullable();
+            $table->string('accepted_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ballots');
+    }
+};
