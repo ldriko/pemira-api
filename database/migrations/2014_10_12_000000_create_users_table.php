717815b1c2c5b181644511016e4834a1dfd399d8
jerryrc <132052046+JERRY-RAMADHANI@users.noreply.github.com>
2023-11-15 17:16:50 +0700

    add provider id in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->string('npm')->primary();
             $table->string('name');
+            $table->string('provider_id')->nullable();
             $table->string('picture')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
